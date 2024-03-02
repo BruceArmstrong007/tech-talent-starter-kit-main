e37a67febdb5cd98d62bbf505a24fde486a06e06
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-03-02 18:51:11 +0530

    starting unit test

--- libs/ng/ui/components/src/lib/color-grid-select/color-grid-select.component.spec.ts
+++ libs/ng/ui/components/src/lib/color-grid-select/color-grid-select.component.spec.ts
@@ -1,5 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
 import { ColorGridSelectComponent } from './color-grid-select.component';
+import {
+  COLOR_GRID_ITEM_SIZES,
+  ColorGridItemSize,
+} from './item';
+import { input } from '@angular/core';
+import {
+  COLOR_GRID_SELECT,
+  ColorGridItemComponent,
+  COLOR_GRID_ITEMS,
+} from './item/item.component';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 describe('ColorGridSelectComponent', () => {
   let component: ColorGridSelectComponent;
@@ -7,15 +21,33 @@ describe('ColorGridSelectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ColorGridSelectComponent],
+      imports: [ColorGridSelectComponent, ColorGridItemComponent],
+      providers: [
+        {
+          provide: NG_VALUE_ACCESSOR,
+          multi: true,
+          useExisting: ColorGridSelectComponent,
+        },
+        {
+          provide: COLOR_GRID_SELECT,
+          useExisting: ColorGridSelectComponent,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ColorGridSelectComponent);
     component = fixture.componentInstance;
+    component.items = input<string[]>(COLOR_GRID_ITEMS);
+    component.itemSize = input<ColorGridItemSize>(COLOR_GRID_ITEM_SIZES[0]);
+    component.disabled = input(false);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should bind inputs correctly', () => {
+  //   expect(component.itemSize()).toBe(COLOR_GRID_ITEM_SIZES[0]);
+  // });
 });
